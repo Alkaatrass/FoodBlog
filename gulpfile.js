1c90c4953a86723236313c40c9720ef2b994a02a
Alkaatrass <seryoga.zonovw@gmail.com>
2023-07-18 11:13:15 +0300

    Исправления для потока записи

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,11 @@
 const { src, dest, watch, series, parallel } = require('gulp');
 
 const html = () => {
-  return src('.src/html/*.html').pipe(dest('./public'));
+  return src('./src/html/*.html').pipe(dest('./public'));
 };
 
 const watcher = () => {
-  watch('./src/html/*.html');
+  watch('./src/html/*.html', html);
 };
 
 exports.html = html;

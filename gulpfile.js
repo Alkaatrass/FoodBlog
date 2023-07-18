75aae0cbcf391383ba0c24abcac7b77c90d1f139
Alkaatrass <seryoga.zonovw@gmail.com>
2023-07-18 10:39:43 +0300

    Сделал наблюдателя

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,13 @@
-const { src, dest } = require('gulp');
+const { src, dest, watch, series, parallel } = require('gulp');
 
 const html = () => {
   return src('.src/html/*.html').pipe(dest('./public'));
 };
 
+const watcher = () => {
+  watch('./src/html/*.html');
+};
+
 exports.html = html;
+
+exports.dev = series(html, watcher);

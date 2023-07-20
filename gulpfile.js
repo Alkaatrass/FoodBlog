bc6c067ddd801fa874c9aaa239797740b1856462
Alkaatrass <seryoga.zonovw@gmail.com>
2023-07-20 10:32:20 +0300

    Сделал синхронизацию с браузером

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,26 @@
 const { src, dest, watch, series, parallel } = require('gulp');
+const browserSync = require('browser-sync').create();
+const devIp = require('dev-ip');
 
 const html = () => {
-  return src('./src/html/*.html').pipe(dest('./public'));
+  return src('./src/html/*.html')
+    .pipe(dest('./public'))
+    .pipe(browserSync.stream());
 };
 
 const watcher = () => {
   watch('./src/html/*.html', html);
 };
 
+const server = () => {
+  browserSync.init({
+    server: {
+      baseDir: './public',
+    },
+    host: devIp(),
+  });
+};
+
 exports.html = html;
 
-exports.dev = series(html, watcher);
+exports.dev = series(html, parallel(server, watcher));
